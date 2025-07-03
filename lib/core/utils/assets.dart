3914ba6098a98e154d976e8d895c91a89bee6d37
HaikalE <muhammadhaikalrahman81@gmail.com>
2025-07-03 23:00:03 +0700

    Add assets: SVG icons, images folder, and assets utility
    - Add food.svg and home.svg category icons
    - Create assets folder structure (images/)
    - Add Assets utility class for asset path management
    - Update pubspec.lock for dependency consistency

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,10 @@
+class Assets {
+  // Logos
+  static const String appLogo = 'assets/images/logos/app_logo.svg';
+  
+  // Category Icons
+  static const String foodIcon = 'assets/icons/categories/food.svg';
+  
+  // Navigation Icons
+  static const String homeIcon = 'assets/icons/navigation/home.svg';
+}
